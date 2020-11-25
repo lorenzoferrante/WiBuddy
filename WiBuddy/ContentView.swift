f84ddf48e02c605aa16fa130372892f15d130206
Lorenzo Ferrante <l.ferrante@iungo.it>
2020-11-25 18:23:28 +0100

    Initial Commit

---
+++ WiBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WiBuddy
+//
+//  Created by Lorenzo Ferrante on 11/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WiBuddy/WiBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiBuddyApp.swift
+//  WiBuddy
+//
+//  Created by Lorenzo Ferrante on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct WiBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
